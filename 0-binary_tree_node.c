048a1a1c2e1da6b17a771ffe6e89be68f48ce363
josephnady <jozephnady#gmail.com>
2024-04-11 17:11:10 -0700

    newcommit

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_t - Func that create binary tree node
+ * @parent: Pointer to the parent node
+ * @value: the value assigned to the parent node
+ * Return: Always 0 (success)
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+}
